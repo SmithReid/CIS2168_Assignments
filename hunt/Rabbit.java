dbc9d6a7d20a0f1cc3d16b17da3c11330f0b5b17
Reid Smith <smithreid001@gmail.com>
2021-01-22 11:33:14 -0500

    58%

--- hunt/Rabbit.java
+++ hunt/Rabbit.java
@@ -1,16 +1,22 @@
-import java.util.Arrays;
+// import java.util.Arrays; // Arrays.toString() used for debugging
+import java.lang.Integer;
 
 public class Rabbit extends Animal {
     private boolean haveSeenFox = false;
     private int lastDirectionToFox = 0;
     private boolean adjacentToWall = false;
     private int lastDirectionToAdjacentWall = 0;
+    private int turnsSinceSeenFox = Integer.MAX_VALUE;
+    private int lastDistanceToFox = Integer.MAX_VALUE;
 
     public Rabbit(Model model, int row, int column) {
         super(model, row, column);
     }
 
     int checkAndMove(int base, int[] possibleMoves) {
+        /* checkAndMove() is sort of a wrapper around Model.turn()
+        with the added functionality of checking all moves in an array and choosing
+        the first acceptable option. */
         int i = 0;
         int intendedDirection = Model.turn(base, possibleMoves[i]);
         while (!canMove(intendedDirection)) {
@@ -32,19 +38,25 @@ public class Rabbit extends Animal {
             if (look(i) == Model.FOX) {
                 haveSeenFox = true;
                 lastDirectionToFox = i;
+                turnsSinceSeenFox = 0;
+                lastDistanceToFox = distance(i);
             }
         }
+        turnsSinceSeenFox++;
         for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
             if (look(i) == Model.EDGE && this.distance(i) == 1) {
                 adjacentToWall = true;
                 lastDirectionToAdjacentWall = i;
             }
         }
-        if (haveSeenFox) {
+        if (lastDistanceToFox < 3) {
+            return checkAndMove(lastDirectionToFox, new int[] {3, 5, 4, 2, 6, 1, 7, 0});
+        }
+        if (haveSeenFox && turnsSinceSeenFox < 5) {
             if (adjacentToWall) {
                 return checkAndMove(lastDirectionToAdjacentWall, new int[] {2, 6, 4, 1, 3, 5, 7, 0});
             }
-            return checkAndMove(lastDirectionToFox, new int[] {3, 5, 1, 7, 2, 4, 6, 0});
+            return checkAndMove(lastDirectionToFox, new int[] {3, 5, 4, 1, 7, 2, 6, 0});
         }
         return Model.STAY;
     }
