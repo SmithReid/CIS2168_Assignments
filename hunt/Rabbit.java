e08c90ff8910cc9006e17ac9b36f690674a5bb49
Reid Smith <smithreid001@gmail.com>
2021-01-22 09:52:40 -0500

    Progress. removing S.o.p's for Grader run

--- hunt/Rabbit.java
+++ hunt/Rabbit.java
@@ -4,24 +4,32 @@ public class Rabbit extends Animal {
     private boolean haveSeenFox = false;
     private int lastDirectionToFox = 0;
     private boolean adjacentToWall = false;
-    private int directionToAdjacentWall = 0;
+    private int lastDirectionToAdjacentWall = 0;
 
     public Rabbit(Model model, int row, int column) {
         super(model, row, column);
     }
 
     int checkAndMove(int base, int[] possibleMoves) {
-        for (int i = 0; i < possibleMoves.length; i++) {
-            if (this.canMove(base + possibleMoves[i])) {
-                // System.out.println(base + possibleMoves[i] + " moving.");
-                return base + possibleMoves[i];
+        System.out.println("Start: checkAndMove()");
+        int i = 0;
+        int intendedDirection = Model.turn(base, possibleMoves[i]);
+        while (!canMove(intendedDirection)) {
+            i++;
+            if (i >= possibleMoves.length) {
+                return Model.STAY;
             }
+            intendedDirection = Model.turn(base, possibleMoves[i]);
         }
-        // System.out.println(base + Arrays.toString(possibleMoves) + "Checked. Not moving.");
-        return Model.STAY;
+        System.out.println("Moving: " + intendedDirection);
+        return intendedDirection;
     }
     
     int decideMove() {
+        if (look(lastDirectionToAdjacentWall) != Model.EDGE || 
+            distance(lastDirectionToAdjacentWall) > 1) {
+            adjacentToWall = false;
+        }
         for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
             if (look(i) == Model.FOX) {
                 haveSeenFox = true;
@@ -29,21 +37,18 @@ public class Rabbit extends Animal {
             }
         }
         for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
-            if (look(i) == Model.EDGE && this.distance(i) == 0) {
+            if (look(i) == Model.EDGE && this.distance(i) == 1) {
                 adjacentToWall = true;
-                directionToAdjacentWall = i;
-            }
-        }
-        for (int i = Model.MIN_DIRECTION; i <= Model.MAX_DIRECTION; i++) {
-            if (look(i) == Model.EDGE && this.distance(i) > 0) {
-                adjacentToWall = false;
+                lastDirectionToAdjacentWall = i;
             }
         }
         if (haveSeenFox) {
             if (adjacentToWall) {
-                return checkAndMove(directionToAdjacentWall, new int[] {2, 6});
+                System.out.println("Adjacent to wall.");
+                return checkAndMove(lastDirectionToAdjacentWall, new int[] {2, 6, 4, 1, 3, 5, 7, 0});
             }
-            return checkAndMove(lastDirectionToFox, new int[] {3, 5, 1, 7});
+            System.out.println("NOT adjacent to wall.");
+            return checkAndMove(lastDirectionToFox, new int[] {3, 5, 1, 7, 2, 4, 6, 0});
         }
         return Model.STAY;
     }
