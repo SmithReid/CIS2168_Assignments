f4150e05ff87a6580ca764978333436260005727
Reid Smith <smithreid001@gmail.com>
2021-03-21 16:21:51 -0400

    minor changes, nothing real

--- cheaters_hangman/Main.java
+++ cheaters_hangman/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        if (args[0].toLowerCase().equals("debug")) {
-            System.out.println("Debug mode: ");
+        if (args.length > 0) {
+            if (args[0].toLowerCase().equals("debug")) {
+                System.out.println("Debug mode: ");
+            } 
         } else {
-            System.out.println("Non-debug mode.");   
+                System.out.println("Non-debug mode.");   
         }
     }
 }
--- cheaters_hangman/README.md
+++ cheaters_hangman/README.md
@@ -1,4 +1,5 @@
-Collaborator: Olivia Chaves
+Collaborators: Olivia Chaves, Reid Smith
+Began 3/21/21
 
 To run: compile java source `javac *.java`
     add input file: `text_files.words.txt`, a dictionary for example, one word per line
